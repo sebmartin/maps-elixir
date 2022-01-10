ba965071ec789e4c480220068d5f8defa83eb687
Sebastien Martin <sebmartin@users.noreply.github.com>
2022-01-09 21:53:24 -0500

    proper tests... they fail, but at least they fail properly

--- config/runtime.exs
+++ config/runtime.exs
@@ -1,6 +1,19 @@
 import Config
 
-# coords expected in format "{lat}, {long}"
-config :maps, :coord1, "44.95222, -76.37706"
-config :maps, :coord2, "45.60419, -75.13199"
-config :maps, :output_resolution, 6400
+case config_env() do
+  :prod ->
+    # coords expected in format "{lat}, {long}"
+    config :maps, :coord1, "0.0000, 0.0000"
+    config :maps, :coord2, "0.0000, 0.0000"
+    config :maps, :output_resolution, 1280
+
+  :dev ->
+    config :maps, :coord1, "44.95222, -76.37706"
+    config :maps, :coord2, "45.60419, -75.13199"
+    config :maps, :output_resolution, 6400
+
+  :test ->
+    config :maps, :coord1, "44.95222, -76.37706"
+    config :maps, :coord2, "45.60419, -75.13199"
+    config :maps, :output_resolution, 6400
+end
--- test/maps/config_test.exs
+++ test/maps/config_test.exs
@@ -2,7 +2,111 @@ defmodule Maps.ConfigTest do
   use ExUnit.Case
   # doctest Maps.Config
 
-  test "parse configs" do
-    assert Maps.Config.parse([]) == %{}
+  setup %{} do
+    %{
+      config_env: %Maps.Config{
+        bottom: "44.95222",
+        left: "-76.37706",
+        top: "45.60419",
+        right: "-75.13199",
+        columns: 1,
+        rows: 1,
+        mapbox_access_token: "",
+        output: nil,
+        output_resolution: 6400,
+      }
+    }
+  end
+
+  test "parse env configs", context do
+    assert Maps.Config.parse([]) == context[:config_env]
+  end
+
+  test "parse with only output argument", context do
+    expected = %{context[:config_env] | output: "/output/path"}
+    assert Maps.Config.parse(["/output/path"]) == expected
+  end
+
+  test "parse external config yaml file", context do
+    # TODO write arguments to temp yaml
+    expected = %{context[:config_env] |
+      bottom: "12.3456",
+      left: "-76.5432",
+      top: "13.3456",
+      right: "-77.5432",
+      output: "/output/path",
+      output_resolution: 10000,
+      mapbox_access_token: "super_secret_magical_token",
+    }
+    assert Maps.Config.parse(["-f arguments.yaml /output/path"]) == expected
+  end
+
+  test "parse cli overrides using aliases", context do
+    argsv = [
+      "-c", "12.3456, -76.5432",
+      "-d", "13.3456, -77.5432",
+      "-r", "10000",
+      "-t", "super_secret_magical_token",
+      "/output/path"
+    ]
+    expected = %{context[:config_env] |
+      bottom: "12.3456",
+      left: "-76.5432",
+      top: "13.3456",
+      right: "-77.5432",
+      output: "/output/path",
+      output_resolution: 10000,
+      mapbox_access_token: "super_secret_magical_token",
+    }
+    assert Maps.Config.parse(argsv) == expected
+  end
+
+  test "parse cli overrides using full argument names", context do
+    argsv = [
+      "--coord1", "12.3456, -76.5432",
+      "--coord2", "13.3456, -77.5432",
+      "--resolution", "10000",
+      "--token", "super_secret_magical_token",
+      "/output/path"
+    ]
+    expected = %{context[:config_env] |
+      bottom: "12.3456",
+      left: "-76.5432",
+      top: "13.3456",
+      right: "-77.5432",
+      output: "/output/path",
+      output_resolution: 10000,
+      mapbox_access_token: "super_secret_magical_token",
+    }
+    assert Maps.Config.parse(argsv) == expected
+  end
+
+  test "parse coordinates are always changed to bottom-left and top-right corners", context do
+    top_left = "20.0, -20.0"
+    bottom_right = "10.0, -10.0"
+    argsv = [
+      "-c", top_left,
+      "-d", bottom_right
+    ]
+    expected = %{context[:config_env] |
+      bottom: "10.0",
+      left: "-20.0",
+      top: "20.0",
+      right: "-10.0",
+    }
+    assert Maps.Config.parse(argsv) == expected
+  end
+
+  test "parse coordinates can be swapped", context do
+    bottom_left = "10.0, -20.0"
+    top_right = "20.0, -10.0"
+    expected = %{context[:config_env] |
+      bottom: "10.0",
+      left: "-20.0",
+      top: "20.0",
+      right: "-10.0",
+    }
+    assert Maps.Config.parse(["-c", top_right, "-d", bottom_left]) == expected
+    assert Maps.Config.parse(["-c", bottom_left, "-d", top_right]) == expected
   end
 end
